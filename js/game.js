6afd1acbd07e623d34b8cbd19f467f4feacc1247
Rae <rhine.wan@gmail.com>
2014-10-05 04:06:33 +0000

    random bubble

--- js/game.js
+++ js/game.js
@@ -5,19 +5,34 @@
 * Time: 02:32 AM
 * To change this template use Tools | Templates.
 */
-var canvas = document.getElementById("draw");
+function start() {
+    var canvas = document.getElementById("draw");
 
-var ctx = canvas.getContext("2d");
-	
-canvas.width = 480;
-canvas.height = 640;
+    var ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "black";
-ctx.beginPath();
-ctx.arc(100, 100, 100, 0, Math.PI * 2, true);
-ctx.fill();
+    var background = Sizzle('#game')[0],
+        rect = background.getBoundingClientRect();
 
-ctx.fillStyle = "white";
-ctx.beginPath();
-ctx.arc(100, 100, 50, 0, Math.PI * 2, true);
-ctx.fill(); 
+    canvas.width = rect.width;
+    canvas.height = rect.width;
+
+    maxWidth = rect.width;
+    maxHeight = rect.height;
+    maxR = (rect.height > rect.width) ? rect.width / 2 : rect.height / 2;
+
+    ballX = Math.floor(Math.random() * maxWidth);
+    ballY = Math.floor(Math.random() * maxHeight);
+    ballR = Math.floor(Math.random() * maxR) + 30;
+
+    ctx.fillStyle = 'black';
+    ctx.beginPath();
+    ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2, true);
+    ctx.fill();
+
+    document.getElementById("cover")
+            .addEventListener("click", touchEvent, false);
+
+    console.log('Start Game');
+}
+
+setInterval(function () { start() }, 1000);
